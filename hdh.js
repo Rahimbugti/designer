7b7a738963d01dc2d7712ee38eb1c0d8cd093e70
Rahimbugti <162613347+Rahimbugti@users.noreply.github.com>
2025-06-26 15:35:07 +0500

    Add files via upload

---
+++ hdh.js
@@ -0,0 +1,27 @@
+var arr=[
+    {
+        dp:"https://images.unsplash.com/photo-1503104834685-7205e8607eb9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHx8",
+        story:"https://images.unsplash.com/photo-1749813482475-3c12a8c4a5bd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3NHx8fGVufDB8fHx8fA%3D%3D"
+    },
+     {
+        dp:"https://images.unsplash.com/photo-1749813482475-3c12a8c4a5bd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3NHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://images.unsplash.com/photo-1749813482475-3c12a8c4a5bd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3NHx8fGVufDB8fHx8fA%3D%3D"
+    },
+     {
+        dp:"https://images.unsplash.com/photo-1526510747491-58f928ec870f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8",
+        story:"https://images.unsplash.com/photo-1749813482475-3c12a8c4a5bd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3NHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp:"https://images.unsplash.com/photo-1502323777036-f29e3972d82f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHx8",
+        story:"https://images.unsplash.com/photo-1749813482475-3c12a8c4a5bd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3NHx8fGVufDB8fHx8fA%3D%3D"
+    },
+] 
+
+var clutter = ""
+arr.forEach(function(elem,idx){
+    clutter +=  `<div id="story"> 
+        <img src="${elem.dp}" alt="">
+      </div>`
+
+})
+document.querySelector("#storiyan").innerHTML = clutter
